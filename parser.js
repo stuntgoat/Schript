1cff54a69175b568594dd68c9dee90341efcc8ff
Isaac <hyperneato@gmail.com>
2012-02-28 17:02:25 -0500

    about to create translate.js which will be a generic Scheme parser.

--- parser.js
+++ parser.js
@@ -10,11 +10,13 @@ function pre_parse(tokenized) {
     // nested JavaScript arrays.
     var depth = 0;
     var stack = {};
+    console.log(tokenized);
     for (var i in tokenized) {
+//	console.log("tokenized[i]: ", tokenized[i]);
 	if (predicates.is_lparen(tokenized[i])) {
 	    depth += 1;
 	    stack[depth] = [];
-	} else if (is_rparen(tokenized[i])) {
+	} else if (predicates.is_rparen(tokenized[i])) {
 	    if (depth !== 1) {
 		stack[depth-1].push(stack[depth]);
 		delete stack[depth];
@@ -24,6 +26,7 @@ function pre_parse(tokenized) {
 	    stack[depth].push(tokenized[i]);
 	}
     }
+    console.log("Stack Depth:", stack[depth]);
     return stack[depth];
 }
 
@@ -36,12 +39,11 @@ function ast(parsed) {
     //  {'func': <scheme function>, 'args': <scheme arguments>}, where <scheme
     //  arguments> can be another JavaScript object with the same format.
     var stack = {};
-    var tester = Object.prototype.toString;
     for (var i in parsed) {
 	if(i === '0') {
 	    stack['func'] = parsed[i];
 	    stack['args'] = [];
-	} else if (tester.call(parsed[i]) === '[object Array]') {
+	} else if (predicates.is_array(parsed[i])) {
 	    stack['args'].push(ast(parsed[i]));
 	} else {
 	    stack['args'].push(parsed[i]);
--- test_parser.js
+++ test_parser.js
@@ -3,14 +3,30 @@
 var lexer = require('./lexer.js');
 var parser = require('./parser.js');
 
-var expression = "(- 2(*(- 4 6 -6)-3 7 56)99)";
 
-var tokenized = lexer.tokenize(expression);
+
+// var expression = "(- 2(*(- 4 6 -6)-3 7 56)99)";
+var expression = "(lambda (x y z) (+ x (* y z)))";
+eq2 = "(define fib (lambda (n) (if (= n 0) 0 (if (= n 1) 1 (+ (fib (- n 1)) (fib (- n 2)))))))";
+eq3 = "(lambda (x y z) (* x y z))"
+
+// var tokenized = lexer.tokenize(expression);
+// var parsed = parser.parse(tokenized);
+
+// console.log("lexer.tokenized given Scheme expression:\n%s", expression);
+// console.log("returns:\n%s", JSON.stringify(tokenized));
+// console.log("parser.parse given tokenized:\n%s", JSON.stringify(tokenized));
+// console.log("returns:");
+// console.log(JSON.stringify(parsed));
+
+
+var tokenized = lexer.tokenize(eq3);
 var parsed = parser.parse(tokenized);
 
-console.log("lexer.tokenized given Scheme expression:\n%s", expression);
+console.log("lexer.tokenized given Scheme expression:\n%s", eq3);
 console.log("returns:\n%s", JSON.stringify(tokenized));
 console.log("parser.parse given tokenized:\n%s", JSON.stringify(tokenized));
 console.log("returns:");
 console.log(JSON.stringify(parsed));
+
 
