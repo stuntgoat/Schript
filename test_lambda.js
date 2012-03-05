e923bc23965a4bde22a8292b135b3dad99c5ce42
Isaac <hyperneato@gmail.com>
2012-03-05 01:15:51 -0500

    need to support passing arguments to a lambda

--- test_lambda.js
+++ test_lambda.js
@@ -2,11 +2,11 @@
 
 var t = require('./translate.js');
 
-expression = "(lambda (x y z) (+ x (* y z)))";
+expression = "((lambda (x y z) (+ x (* y z))) 1 2 3)";
 expression1 = "(lambda (x y z) (lambda (a b c) (+ x a (* y b (/ c z)))))";
 
-var a = t.translate(expression1);
-console.log("input:", expression1);
+var a = t.translate(expression);
+console.log("input:", expression);
 console.log("output:", a);
 
 
