88af99c6897b951274d826b439b840d6d9865377
Isaac <hyperneato@gmail.com>
2012-03-08 21:06:16 -0500

    simplified predicates.js

--- predicates.js
+++ predicates.js
@@ -1,4 +1,4 @@
-
+// predicates.js
 
 var tester = Object.prototype.toString;
 
@@ -11,67 +11,44 @@ function is_comma(item) {
 }
 
 function is_lparen(item) {
-    if (item === '(') {
-	return true;
-    } else {
-	return false;
-    }
+    return item === '(';
 }
-exports.is_lparen = is_lparen;
 
 function is_rparen(item) {
-    if (item === ')') {
-	return true;
-    } else {
-	return false;
-    }
+    return item === ')';
 }
-exports.is_rparen = is_rparen;
 
 function is_object(test) {
-    if (tester.call(test) === '[object Object]') { 
-	return true;
-    }
-    return false;
+    return tester.call(test) === '[object Object]';
 }
-exports.is_object = is_object;
 
 function is_array(test) {
-    if (tester.call(test) === '[object Array]') { 
-	return true;
-    }
-    return false;
+    return tester.call(test) === '[object Array]';
 }
-exports.is_array = is_array;
 
 function is_string(test) {
-    if (tester.call(test) === '[object String]') { 
-	return true;
-    }
-    return false;
+    return tester.call(test) === '[object String]';
 }
-exports.is_string = is_string;
 
 function is_number(test) {
-    if (tester.call(test) === '[object Number]') { 
-	return true;
-    }
-    return false;
+    return tester.call(test) === '[object Number]';
 }
-exports.is_number = is_number;
 
 function is_function(test) {
-    if (tester.call(test) === '[object Function]') { 
-	return true;
-    }
-    return false;
+    return tester.call(test) === '[object Function]';
 }
-exports.is_function = is_function;
 
 function is_boolean(test) {
-    if (tester.call(test) === '[object Boolean]') { 
-	return true;
-    }
-    return false;
+    return tester.call(test) === '[object Boolean]';
 }
+
+exports.is_backquote = is_backquote;
+exports.is_comma = is_comma;
+exports.is_lparen = is_lparen;
+exports.is_rparen = is_rparen;
+exports.is_object = is_object;
+exports.is_array = is_array;
+exports.is_string = is_string;
+exports.is_number = is_number;
+exports.is_function = is_function;
 exports.is_boolean = is_boolean;
