1253f8ba61c5cbc884171279191c50bc451e4391
Isaac <hyperneato@gmail.com>
2012-03-17 17:28:37 -0400

    basic arithmetic handling

--- lexer.js
+++ lexer.js
@@ -7,71 +7,103 @@
 
 // Tuesday - lexer lookup table: finish
 
+function token_identity(token) {
+    return token;
+}
 
 var match_table = [
     {
-        regex_str: "(\\'?\\d*[\\w_-][\\d\\w_-]+)", // quoted symbol
-        handler_f:  function(token) { return {'token': 'symbol', attribute: 'quoted'};}
+        regex_str: "([a-zA-Z_]+)", // symbol
+        handler_f: token_identity
     },
     {
-        regex_str: "(\\,?\\d*[\\w_-][\\d\\w_-]+)", // symbol
-        handler_f:  function(token) { return {'token': 'symbol', attribute: 'unquoted'};} 
-    },  
-    {
-        regex_str:"(\\d+[\\.]?\\d*)", // numbers
-        handler_f: function(token) { return {'token': 'NUMBER', value: token };}
+        regex_str:"(-?\\d+)", // number
+        handler_f: function(token) { return parseInt(token);}
     },
     {
         regex_str: "(\\+)", // addition
-        handler_f: function(token) { return {'token': 'ADD'};}
+        handler_f: token_identity
     },
     {
         regex_str: "(\\-)", // subtraction
-        handler_f: function(token) { return {'token': 'SUB'};}
+        handler_f: token_identity
     },
     {
         regex_str: "(\\*)", // multiplication
-        handler_f: function(token) { return {'token': 'MUL'};}
+        handler_f: token_identity
     },
     {
         regex_str: "(\\/)", // division
-        handler_f: function(token) { return {'token': 'DIV'};}
+        handler_f: token_identity
     },
     {
-        regex_str: "(`\\()", // backquoted left paren
-        handler_f: function(token) { return {'token': 'L_PAREN', attribute: 'backquoted'};}
+        regex_str: "(\\>)", // greater than
+        handler_f: token_identity
     },
     {
-        regex_str: "(,\\()", // comma un-escaping left paren
-        handler_f: function(token) { return {'token': 'L_PAREN', attribute: 'escaped'};}
+        regex_str: "(\\<)", // less than
+        handler_f: token_identity
     },
     {
-        regex_str: "(\\()", // left paren unquoted
-        handler_f: function(token) { return {'token': 'L_PAREN', attribute: 'unquoted'};}
+        regex_str: "(\\=)", // equal sign
+        handler_f: token_identity
     },
     {
-        regex_str: "(\\))", // right paren unquoted
-        handler_f: function(token) { return {'token': 'R_PAREN', attribute: 'unquoted'};}
+        regex_str: "(\\@)", // equal sign
+        handler_f: token_identity
     },
     {
-        regex_str: "(\\'\\()", // quoted left paren
-        handler_f: function(token) { return {'token': 'L_PAREN', attribute: 'quoted'};}
+        regex_str: "(\\!)", // exclamation mark
+        handler_f: token_identity
+    },
+    {
+        regex_str: "(\\;)", // semi-colon
+        handler_f: token_identity
+    },
+    {
+        regex_str: "(\\|)", // pipe
+        handler_f: token_identity
+    },
+    {
+        regex_str: "(\\()", // left paren
+        handler_f: token_identity
+    },
+    {
+        regex_str: "(\\))", // right paren
+        handler_f: token_identity
+    },
+    {
+        regex_str: "(\\')", // single quote
+        handler_f: token_identity
+    },
+    {
+        regex_str: '(\\")', // double quote
+        handler_f: token_identity
+    },
+    {
+        regex_str: "(\\`)", // backquote
+        handler_f: token_identity
+    },
+    {
+        regex_str: "(\\,)", // comma
+        handler_f: token_identity
     }
 ];
-exports.match_table = match_table;
+
 
 // TODO: better way to push tokens
 // 
 var match_indexer = match_table.length; // first and last two elements are not needed
 var match_index_regex = /\d+/;
 
-function tokenize(expression) {
+
+var tokenize = exports.tokenize = function tokenize(expression) {
     var tokens = [];    
     // map creates a new array, by calling the callback with each element in the map_table
     // apply calls join on the array created by match_table.map; the arguments to join are in an array- in this case '|'
-    var re_str_array = match_table.map(function () { return arguments[0].regex_str;});
+    var re_str_array = match_table.map(function (elem) { return elem.regex_str;});
     var regex_string = re_str_array.join('|');
-    console.log(regex_string);    
+    //    console.log(regex_string);    
     var regex = new RegExp(regex_string, 'g');
     
     expression.replace(regex, function() {
@@ -87,11 +119,3 @@ function tokenize(expression) {
 }                             
 
 
-var input = "('foo + 2.90 3 )"; //(`(html (head (body (p ,(+ 2 2))
-console.log('input: ', input);
-console.log('output: ', tokenize(input));
-exports.tokenize = tokenize;
- 
-    
-     
-  
--- parser.js
+++ parser.js
@@ -2,73 +2,129 @@
 // Functions to create an abstract syntax tree from an array of Scheme tokens.
 // exports: parse
 var predicates = require('./predicates.js');
+var assert = require('assert');
+
+// null 'equals' [], that is they should be interchangable
+
+// ("cat")
+var string_cat = [['STRING', 'cat'], null];
+
+// '(a 7 "cat")
+var cat = ['QUOTE', ['a', 7, ['STRING', 'cat'], null]];
+
+// ('a 7 "cat")
+var unquoted_cat =  [['QUOTE', 'a'], 7, ['STRING', 'cat'], null];
+
+// (+ 5 6 (- 8 3))
+var arith_1 = ['+', 5, 6, ['-', 8, 3, null], null];
+
+// (define foo 2)
+var define_1 = ['define', 'foo', 2, null];
+
+// ((lambda (x) (* x x)) 2)
+var lambda_1 = [['lambda', ['x', null], ['*', 'x', 'x', null]], 2, null];
+
+// (lambda () (* 90 3))
+var lambda_2 = ['lambda', [null], ['*', 90, 3, null], null];
+
+// (list 2 4 5)
+var list_1 = ['list', 2, 4, 5, null];
+// eval(cons(2, cons(4, cons(5, null)
+
+// (if (< 2 5 8) 0 1)
+var if_1 = ['if', ['<', 2, 5, 8, null], 0, 1, null];
+// if = if eval(cadr if_node), eval(caddr), else cadddr.
 
 
 
-
-var lexemes = {
-    
-    '(': 'l_paren',
-    ')': 'r_paren',
-    '+': 'plus_op',
-    '-': 'minus_op',
-    '*': 'mult_op',
-    '/': 'div_op',
-    
-};
-
-var symbol_table = {
-    'if': {},
-    'lambda': {},
-    'define': {},
-    'defmacro': {}
-};
-
-function lexer(input_string) {
-    var tokens = [];
-    var i;
-    for (i=0; i < input_string.length; i++) {
-                
-
-
-
+function car(sexp) {
+    if (sexp.length) {
+	return sexp[0];
+    } else {
+	return false;
     }
+}
 
+function cdr(sexp) {
+    var cdr_sexp;
+    if (sexp.length > 2) {
+	cdr_sexp = sexp.slice();
+	cdr_sexp.splice(0, 1);
+	return cdr_sexp;
+    } else {
+	return false;
+    }
+}
 
+function cons(exp1, exp2) {
+    if (predicates.is_array(exp2)) {
+	exp2.unshift(exp1);
+	return exp2;
+    } else {
+	return false;
+    }
+}
 
-    return tokens;
+function ast_to_js(sexp) {
+
+    if (bindings[car(sexp)]) {
+	return bindings[car(sexp)](cdr(sexp));
+    } else {
+	// pass to form handlers
+    }
+    throw new Error('in ' + sexp + ' ' + car(sexp) + ' not supported');
 }
 
 
 
-
-
-
-
-
+var bindings = {
+    '+': generate_math_operator("+"),
+    '-': generate_math_operator("-"),
+    '*': generate_math_operator("*"),
+    '/': generate_math_operator("/"),
+    '<': generate_compare('<'),
+    '>': generate_compare('>'),
+    '>=': generate_compare('>='),
+    '<=': generate_compare('<=')
+};
 
 function generate_math_operator(op) {
-  return function(args) {
-    var res = args[0], i, tmp;
-    for (i=1; i<args.length; i++) {
-      tmp = predicates.is_object(args[i]) ? ast_to_js(args[i]) : args[i];
-      res += " " + op + " " + tmp;
-    }
-    return '(' + res + ')';
-  };
+    return function(args) {
+	assert.deepEqual(true, (args.length >= 2));
+	var i, tmp, first, last,
+        statement = '',
+	first = args[0];
+	
+	statement += predicates.is_array(first) ? ast_to_js(first) : first;
+	for (i=1; i<args.length; i++) {
+	    tmp = predicates.is_array(args[i]) ? ast_to_js(args[i]) : args[i];
+	    if (i === (args.length - 1)) {
+		if (predicates.is_null(tmp)) {
+		    break;
+		} else if (predicates.is_array(tmp)) {
+		    statement += (op + ast_to_js(last));
+		} else {
+		    throw new Error(args[i] + ', last item in a list, is not an s-expression or null');
+		}
+		break;
+	    }
+	    statement += (op + tmp);
+	}
+	return '(' + statement + ')';
+    };
 }
 
 function generate_compare(op) {  
     return function (args) {
-        var res = '';
-        var i = 0; 
+        var res = '',
+        i = 0; 
         if (args.length < 2) {
-            throw 'too few args';
-        }
-        do {
-            res += '(' + args[i] + ' ' + op + ' ' + args[i+1] + ')';
-            i++;
-            if (i < args.length - 1) {
+            throw new Error('too few args:');
+	}
+ 	do {
+	    res += '(' + args[i] + ' ' + op + ' ' + args[i+1] + ')';
+	    i++;
+	    if (i < args.length - 1) {
                 res += ' && ';
             }
         } while (i < args.length - 1)
@@ -76,194 +132,13 @@ function generate_compare(op) {
     };
 }
 
-var bindings = {
-    '+': generate_math_operator("+"),
-    '-': generate_math_operator("-"),
-    '*': generate_math_operator("*"),
-    '/': generate_math_operator("/"),
-    'list': function (args) {
-        return '[' + args.toString() + ']';
-    },
-    '<': generate_compare('<'),
-    '>': generate_compare('>'),
-    '>=': generate_compare('>='),
-    '<=': generate_compare('<=')
-};
+// (+ 5 2 (- 3 6 8))
+var arith_2 = ['+', 5, 2, ['-', 3, 6, 8, null], null];
+var arith_3 = ['<', 1, 2, ['+', 3, 4, null], null];
 
-function ast_to_js(node) {
-    return form_handlers[node.form](node);
-}
+console.log(ast_to_js(arith_2));
 
-var form_handlers = {
-    normal: function (node) {
-        if (node.proc.symb) {
-            return bindings[node.proc.symb](node.args);
-        } else {
-            return '(' + ast_to_js(node.proc) + ')(' + node.args.toString() + ')';
-        }
-    },
-    lambda: function (node) {
-        return 'function (' + node.args.toString() + ') { return ' + ast_to_js(node.expr) + ';}';
-    },
-    'if': function (node) {
-        return '(function () { if (' + ast_to_js(node.cond) + ') { return ' + ast_to_js(node.texp) + '; } else { return ' + ast_to_js(node.fexp) + '; } })()';},
-    identity: function (node) {
-        return node.ival;
-    },
-    define: function (node) {
-        return "var " + node.symb + " = " + ast_to_js(node.expr) + ";";
-    }
-};
+exports.car = car;
+exports.cdr = cdr;
+exports.cons = cons;
 
-// (defmacro ham (a b c) `(+ ,a (- ,b ,c)))
-var dm_ast = {
-    form: 'defmacro',
-    symb: 'ham',
-    args: ['a', 'b', 'c'],
-    mexp: ['`(', '+', ',a', '(', '-', ',b', ',c', ')', ')', ')']
-};
-
-var defmacro = function (node) {
-    // create a function that will accept variables and apply them to a copy of mexp
-    var macro = function (args) {
-        var mexp = node.mexp; // a copy of the unexpanded macro form
-        // an object matching defmacro's declared args to passed-in args at macro call
-        var arg_pairs = tools.zip(node.args, args).reduce(function (acc, val) { acc[val[0]] = val[1]; return acc}, {});
-
-        // expand macro expression by substituting all args with passed-in vals;
-        // reduce unquoted expressions         
-        
-        // create AST from expanded generated Scheme string
-
-        // call ast_to_js on AST
-        
-    };
-    // place macro function inside local copy of form_handlers
-    // TODO: check that macro name is not in form_handlers; if it is, throw error
-    form_handlers[node.symb] = macro;
-};
-
-// write function that parses a macro expression and reduces unquoted expressions- s-expressions unquoted by ',(' 
-
-// ex. input: ['`(', '+', '5', '3', ',(', '*', 'a', 'b', ')', ',a', '99', ')']
-// ex. passed-in args {a:'22', b:'88'} output: '(+ 5 3 1936 22 99)'
-
-function parse_macro_expression(mexp) {
-    // substitute all unquoted passed-in args with args that were actually passed in
-    // if any s-exp is unquoted, evaluate this expression 
-    var expanded = '';
-    
-
-}
-
-
-console.log("\n\n");
-// (define foo 2)
-var def_ast = {
-  form: "define",
-  symb: "foo",
-  expr: {
-    form: "identity",
-    ival: "2"
-  }
-};
-console.log("~input: (define foo 2)");
-console.log("output: ", ast_to_js(def_ast));
-console.log("\n\n");
-
-var lam = {
-        form: "lambda",
-        args: [],
-        expr: {
-            form: "normal",
-            proc: {
-                form: "symbol",
-                symb: "*"
-            },
-            args: ['90', '3']
-        }
-};
-console.log("~input: (lambda () (* 90 3)");
-console.log("output: ", ast_to_js(lam));
-console.log("\n\n");
-
-// var m = "(+ 5 6 (- 8 3))";
-
-var list_ast = {
-    form: 'normal',
-    proc: {
-        form: 'symbol',
-        symb: 'list'
-    },
-    args: ['2', '4', '6']
-};
-
-console.log("~input: (list 2 4 5)");
-console.log("output: ", ast_to_js(list_ast));
-console.log("\n\n");
-
-// (if (< 2 5 8) 0 1)
-var if_ast = {
-  form: "if",
-  cond: {
-    form: "normal",
-    proc: {
-      form: "symbol",
-      symb: "<"
-    },
-    args: ["2", "5", "8"]
-  },
-  texp: {
-    form: "identity",
-    ival: "0"
-  },
-  fexp: {
-    form: "identity",
-    ival: "1"
-  }
-};
-
-console.log("~input: (if (< 2 5 8) 0 1)");
-console.log("output: ", ast_to_js(if_ast));
-console.log("\n\n");
-
-var m_ast = {
-    form: 'normal',
-    proc: {
-        form: 'symbol',
-        symb: '+'
-    },
-    args: ['5', '6', {
-               form: 'normal',
-               proc: {
-                   form: 'symbol',
-                   symb: '-'
-               },
-               args: ['8', '3']
-           }]
-};
-console.log("~input: (+ 5 6 (- 8 3))");
-console.log("output: ", ast_to_js(m_ast));
-console.log("\n\n");
-
-// example of lambda:n
-// ((lambda (x) (* x x)) 2)
-var lambda = {
-    form: "normal",
-    proc: {
-        form: "lambda",
-        args: ["x"],
-        expr: {
-            form: "normal",
-            proc: {
-                form: "symbol",
-                symb: "*"
-            },
-            args: ['x', 'x']
-        }
-    },
-    args: ['2']
-};
-console.log("~input:((lambda (x) (* x x)) 2) ");
-console.log("output: ", ast_to_js(lambda));
-console.log("\n\n");
