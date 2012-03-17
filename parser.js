50bf57382298b9673ba5b2b5a19d3f0a30800c82
Isaac <hyperneato@gmail.com>
2012-03-17 10:26:29 -0400

    incremental backup

--- parser.js
+++ parser.js
@@ -5,8 +5,11 @@ var predicates = require('./predicates.js');
 ////////////////////////////////////////////////////////////////////////////////
 // BEGIN PARSER
 
+// null 'equals' [], that is they should be interchangable
+
 // ("cat")
 var string_cat = [['STRING', 'cat'], null];
+
 
 // '(a 7 "cat")
 var cat = ['QUOTE', ['a', 7, ['STRING', 'cat'], null]];
@@ -28,9 +31,11 @@ var lambda_2 = ['lambda', [null], ['*', 90, 3, null], null];
 
 // (list 2 4 5)
 var list_1 = ['list', 2, 4, 5, null];
+// eval(cons(2, cons(4, cons(5, null)
 
 // (if (< 2 5 8) 0 1)
 var if_1 = ['if', ['<', 2, 5, 8, null], 0, 1, null];
+// if = if eval(cadr if_node), eval(caddr), else cadddr.
 
 
 // END PARSER
